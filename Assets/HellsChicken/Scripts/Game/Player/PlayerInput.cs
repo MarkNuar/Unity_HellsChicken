bd987093046e6545ab98367647cc9238575abf09
Tallino <gdanese@hotmail.it>
2020-10-18 18:21:00 +0200

    Reordered folders and scripts
    PlayerInput has now the Weapon code
    Moved FireController in a folder

--- Assets/HellsChicken/Scripts/Game/Player/PlayerInput.cs
+++ Assets/HellsChicken/Scripts/Game/Player/PlayerInput.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public Transform _firePoint;
+       [SerializeField] [Range(0.1f,5.0f)] private float _waitBetweenFires = 2f;
+       public GameObject firePrefab;
+       private bool _canShoot = true;
+   
+       private void Start()
+       {
+           _canShoot = true;
+       }
+   
+       // Update is called once per frame
+       void Update()
+       {
+           if (Input.GetButtonDown("Fire1") && _canShoot==true)
+           {           
+               _canShoot = false;
+               Shoot();
+               StartCoroutine(EnableFire(_waitBetweenFires));
+           }
+       }
+   
+       public void Shoot()
+       {
+           Instantiate(firePrefab, _firePoint.position, _firePoint.rotation);
+       }
+       
+       IEnumerator EnableFire(float time)
+       {
+           yield return new WaitForSeconds(time);
+           _canShoot = true;
+           
+           yield return null;
+       }
 }
