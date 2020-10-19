31ae5e865e448c24cde1cf0a4c5835db708c86cc
MarkNuar <marco.caruso98@gmail.com>
2020-10-19 13:02:53 +0200

    Last commit before Character Controller change

--- Assets/HellsChicken/Scripts/Game/Player/PlayerController.cs
+++ Assets/HellsChicken/Scripts/Game/Player/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace HellsChicken.Scripts.Game.Player
 {
@@ -7,12 +8,14 @@ namespace HellsChicken.Scripts.Game.Player
     public class PlayerController : MonoBehaviour
     {
 
-        [SerializeField] private float _walkingSpeed = 5.0f;
-        [SerializeField] private float _jumpSpeed = 5.0f;
+        [SerializeField] private float _walkSpeed = 5.0f;
+        [SerializeField] private float _jumpFactor = 5.0f;
 
         private float _horizontalMovement;
         private bool _jump = false;
 
+        private Vector3 _tempVelocity;
+        
         private CapsuleCollider _capsuleCollider;
         private Rigidbody _rigidbody;
         private Transform _transform;
@@ -24,6 +27,7 @@ namespace HellsChicken.Scripts.Game.Player
             _capsuleCollider = gameObject.GetComponent<CapsuleCollider>();
             _rigidbody = gameObject.GetComponent<Rigidbody>();
             _transform = gameObject.GetComponent<Transform>();
+            _tempVelocity = new Vector3();
         }
 
         public void MoveHorizontally(float horizontalMovement)
@@ -33,15 +37,7 @@ namespace HellsChicken.Scripts.Game.Player
 
         public void Jump()
         {
-            if (IsGrounded())
-            {
-                _jump = true;
-            }
-            else
-            {
-                _jump = false;
-            }
-
+            _jump = IsGrounded();
         }
     
 
@@ -49,13 +45,23 @@ namespace HellsChicken.Scripts.Game.Player
         {
             if (_jump)
             {
-                _rigidbody.AddForce(_jumpSpeed*_transform.up,ForceMode.Impulse);
+                _tempVelocity.y = _jumpFactor;
+                //_rigidbody.AddForce(_jumpFactor*_transform.up,ForceMode.Impulse);
                 _jump = false;
             }
-            _rigidbody.MovePosition(_rigidbody.position + _walkingSpeed * _horizontalMovement * Time.fixedDeltaTime * (Vector3)_transform.right);
+            else
+            {
+                _tempVelocity.y = _rigidbody.velocity.y;
+            }
+            //Speed update 
+            _tempVelocity.x = _walkSpeed * _horizontalMovement;
+            _tempVelocity.z = 0f;
+            _rigidbody.velocity = _tempVelocity;
+            //_rigidbody.MovePosition(_rigidbody.position + _walkSpeed * _horizontalMovement * Time.fixedDeltaTime * (Vector3)_transform.right);
             _horizontalMovement = 0f;
+            
         }
-
+        
         private bool IsGrounded()
         {
             //return Physics.SphereCast(_transform.position, _capsuleCollider.bounds.extents.x, -Vector3.up, out _hitInfo, _capsuleCollider.bounds.extents.y + 0.1f);
