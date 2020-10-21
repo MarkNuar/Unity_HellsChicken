6e63e0ef179e25a7526d47ca8d98bc13950f52a8
MarkNuar <marco.caruso98@gmail.com>
2020-10-21 11:32:23 +0200

    Player Refactor

--- Assets/HellsChicken/Scripts/Game/Player/PlayerController.cs
+++ Assets/HellsChicken/Scripts/Game/Player/PlayerController.cs
@@ -16,9 +16,8 @@ namespace HellsChicken.Scripts.Game.Player
         [SerializeField] private float _jumpSpeed = 5.0f;
         [SerializeField] private float _gravityScale = 2.0f;
         [SerializeField] private float _fallingGravityIncrease = 1.0f;
-        [SerializeField] private float _glidingUpGravityIncrease = 5.0f;
-        //deprecated
-        //[SerializeField] private float _glidingDownGravityDecrease = 0.1f;
+        // [SerializeField] private float _glidingUpGravityIncrease = 5.0f;
+        
 
         [SerializeField] private float _maxVerticalSpeed = 40f;
         [SerializeField] private float _glidingDescentFixedSpeed = 15f;
@@ -27,28 +26,30 @@ namespace HellsChicken.Scripts.Game.Player
         
         private float _gravity;
 
-        private float _horizontalMovement;
-        private bool _jump = false;
-        private bool _glide;
-
+        // private float _horizontalMovement;
+        // private bool _jump = false;
+        // private bool _glide;
+        
+        
         private Vector3 _moveDirection;
 
-        private Transform _transform;
+        // private Transform _transform;
         private CharacterController _characterController;
-        private MeshRenderer _meshRenderer;
+        // private MeshRenderer _meshRenderer;
 
-        [SerializeField] private Material _glideMaterial;
-        [SerializeField] private Material _normalMaterial;
+        // [SerializeField] private Material _glideMaterial;
+        // [SerializeField] private Material _normalMaterial;
 
         private void Awake()
         {
             _characterController = gameObject.GetComponent<CharacterController>();
-            _transform = gameObject.GetComponent<Transform>();
-            _meshRenderer = gameObject.GetComponent<MeshRenderer>();
+            // _transform = gameObject.GetComponent<Transform>();
+            // _meshRenderer = gameObject.GetComponent<MeshRenderer>();
             _moveDirection = Vector3.zero;
             _gravity = - Physics.gravity.y;
         }
-
+        
+        /*
         public void MoveHorizontally(float horizontalMovement)
         {
             _horizontalMovement = horizontalMovement;
@@ -61,6 +62,8 @@ namespace HellsChicken.Scripts.Game.Player
         {
             _glide = IsFalling() && !_characterController.isGrounded;
         }
+        */
+        
         public void ShootFlames()
         {
             Instantiate(_fireStreamPrefab, _firePosition.position, _firePosition.rotation);
@@ -72,9 +75,9 @@ namespace HellsChicken.Scripts.Game.Player
             Debug.Log("Start Egg Aiming");
         }
         
-        private void FixedUpdate()
+        private void Update()
         {
-            
+            /*   
             _moveDirection.x = _horizontalMovement * _walkSpeed;
             _moveDirection.z = 0f;
             if (_characterController.isGrounded)
@@ -121,62 +124,67 @@ namespace HellsChicken.Scripts.Game.Player
             if (Math.Abs(_moveDirection.y) > _maxVerticalSpeed)
                 _moveDirection.y = Math.Sign(_moveDirection.y) * _maxVerticalSpeed;
             _characterController.Move(_moveDirection * Time.fixedDeltaTime);
+            */
+            if(Input.GetButtonDown("Fire1"))
+                ShootFlames();
+            if(Input.GetButtonDown("Fire2"))
+                StartEggAiming();
             
-            
-            
-            
-            //TODO VERSION WITHOUT PLAYER INPUT, IT ONLY INCREASES COMPLEXITY
-            //TODO add controls fetch in the Update function!!
-            /*
             _moveDirection.x = Input.GetAxis("Horizontal") * _walkSpeed;
             _moveDirection.z = 0f;
-            if (_characterController.isGrounded)
+            if (IsGrounded())
             {
-                _moveDirection.y = -10f;
+                _moveDirection.y = -20f;
                 if (Input.GetButtonDown("Jump"))
                 {
                     _moveDirection.y = _jumpSpeed;
-                    //CALL ANIMATOR FOR JUMP ANIMATION
                 }
             }
-            else //not grounded, so falling or gliding
+            else
             {
                 if (IsFalling())
                 {
-                    if (!Input.GetButton("Jump"))
+                    if (true)//!Input.GetButton("Jump")) //TODO
                     {
+                        //FALLING DOWN AT A FASTER SPEED
                         _moveDirection.y -= _fallingGravityIncrease * _gravityScale * _gravity;
                     }
                     else
                     {
+                        //GLIDING DOWN
                         _moveDirection.y = -_glidingDescentFixedSpeed;
                     }
                 }
                 else
                 {
+                    //GOING UP AT A SLOWER SPEED
                     _moveDirection.y -= _gravityScale * _gravity;
                 }
             }
-            //Clamping _moveDirection.y at _maxVerticalSpeed
+            //CLAMPING MAX SPEED
             if (Math.Abs(_moveDirection.y) > _maxVerticalSpeed)
                 _moveDirection.y = Math.Sign(_moveDirection.y)* _maxVerticalSpeed;
-            _characterController.Move(_moveDirection * Time.fixedDeltaTime);
-            */
+            _characterController.Move(_moveDirection * Time.deltaTime);
         }
 
-        public bool IsGrounded()
+        
+        private bool IsGrounded()
         {
             return _characterController.isGrounded;
         }
 
-        public bool IsGliding()
-        {
-            return _glide;
-        }
         private bool IsFalling()
         {
-            return _moveDirection.y < 0;
+            return _moveDirection.y < 0f ;
         }
+        
+        //DEPRECATED
+        // public bool IsGliding()
+        // {
+        //     return _glide;
+        // }
+
+        
         private void OnControllerColliderHit(ControllerColliderHit hit)
         {
             if (_characterController.collisionFlags != CollisionFlags.Above) return;
--- Assets/HellsChicken/Scripts/Game/Player/PlayerInput.cs
+++ Assets/HellsChicken/Scripts/Game/Player/PlayerInput.cs
@@ -8,16 +8,18 @@ namespace HellsChicken.Scripts.Game.Player
 
     public class PlayerInput : MonoBehaviour
     {
-        private PlayerController _playerController;
+        //TODO: REMOVE THIS SCRIPT, NOW USELESS
+        // private PlayerController _playerController;
+        //
+        // private float _horizontalMovement; //this will be 1 for right, -1 for left
+        // private bool _jump;
+        // private bool _glide;
+        // private bool _shootFlames;
+        // private bool _startEggAiming;
         
-        private float _horizontalMovement; //this will be 1 for right, -1 for left
-        private bool _jump;
-        private bool _glide;
-        private bool _shootFlames;
-        private bool _startEggAiming;
         private void Awake()
         {
-            _playerController = gameObject.GetComponent<PlayerController>();
+            //_playerController = gameObject.GetComponent<PlayerController>();
         }
 
         private void OnEnable()
@@ -40,48 +42,47 @@ namespace HellsChicken.Scripts.Game.Player
         // Update is called once per frame
         void Update()
         {
-            /*
-            //_horizontalMovement = _newInputSystem.Walking.Move.ReadValue<float>();
-            _horizontalMovement = Input.GetAxisRaw("Horizontal");
-            if (Input.GetButtonDown("Jump"))
-                _jump = true;
-            if (Input.GetButton("Jump"))
-                _glide = true;
-            if (Input.GetButtonDown("Fire1"))
-                _shootFlames = true;
-            if (Input.GetButtonDown("Fire2"))
-                _startEggAiming = true;
-            */
-            //TODO: END EGG AIMING.
-            //todo : check with get button up if we can detect end of gliding. 
+            //DEPRECATED
+            // /*
+            // //_horizontalMovement = _newInputSystem.Walking.Move.ReadValue<float>();
+            // _horizontalMovement = Input.GetAxisRaw("Horizontal");
+            // if (Input.GetButtonDown("Jump"))
+            //     _jump = true;
+            // if (Input.GetButton("Jump"))
+            //     _glide = true;
+            // if (Input.GetButtonDown("Fire1"))
+            //     _shootFlames = true;
+            // if (Input.GetButtonDown("Fire2"))
+            //     _startEggAiming = true;
+            // */
         }
     
         private void FixedUpdate()
         {
-            //TODO
-            /*
-            _playerController.MoveHorizontally(_horizontalMovement);
-            if (_jump)
-            {
-                _playerController.Jump();
-                _jump = false;
-            }
-            if (_glide)
-            {
-                _playerController.Glide();
-                _glide = false;
-            }
-            if (_shootFlames)
-            {
-                _playerController.ShootFlames();
-                _shootFlames = false;
-            }
-            if (_startEggAiming)
-            {
-                _playerController.StartEggAiming();
-                _startEggAiming = false;
-            }
-            */
+            //DEPRECATED
+            // /*
+            // _playerController.MoveHorizontally(_horizontalMovement);
+            // if (_jump)
+            // {
+            //     _playerController.Jump();
+            //     _jump = false;
+            // }
+            // if (_glide)
+            // {
+            //     _playerController.Glide();
+            //     _glide = false;
+            // }
+            // if (_shootFlames)
+            // {
+            //     _playerController.ShootFlames();
+            //     _shootFlames = false;
+            // }
+            // if (_startEggAiming)
+            // {
+            //     _playerController.StartEggAiming();
+            //     _startEggAiming = false;
+            // }
+            // */
         }
     }
 }
