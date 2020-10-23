5c8c132fd67ad65beb7af6aca6b512d7ae48ede0
ToMmAzO <tommaso4.pozzi@mail.polimi.it>
2020-10-23 10:22:17 +0200

    Add Egg

---
+++ Assets/HellsChicken/Scripts/Game/Player/Egg/Destruction.cs
@@ -0,0 +1,22 @@
+// --------------------------------------
+// This script is totally optional. It is an example of how you can use the
+// destructible versions of the objects as demonstrated in my tutorial.
+// Watch the tutorial over at http://youtube.com/brackeys/.
+// --------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destruction : MonoBehaviour {
+
+	[SerializeField] GameObject destroyedVersion;
+	
+	public void Destroyer ()
+	{
+		Instantiate(destroyedVersion, transform.position, transform.rotation);
+		
+		Destroy(gameObject);
+	}
+
+}
---
+++ Assets/HellsChicken/Scripts/Game/Player/Egg/EggExplosion.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggExplosion : MonoBehaviour
+{
+    private float timer = 2f;
+    private float countdown;
+
+    private bool hasExploded;
+
+    [SerializeField] GameObject explosionEffect;
+    private float radius = 5f;
+    private float force = 500f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = timer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0f && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+    }
+
+    void Explode()
+    {
+        GameObject particle = Instantiate(explosionEffect, transform.position, transform.rotation);
+        Destroy(particle, 1f);
+        
+        Collider[] collidersToMove = Physics.OverlapSphere(transform.position, radius);
+        foreach (Collider nearbyObject in collidersToMove)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, radius);
+            }
+        }
+        
+        Collider[] collidersToDestroy = Physics.OverlapSphere(transform.position, radius);
+        foreach (Collider nearbyObject in collidersToDestroy)
+        {
+            Destruction dest = nearbyObject.GetComponent<Destruction>();
+            if (dest != null)
+            {
+                dest.Destroyer();
+            }
+        }
+
+        Destroy(gameObject);
+    }
+}
