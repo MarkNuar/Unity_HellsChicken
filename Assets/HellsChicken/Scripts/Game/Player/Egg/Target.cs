c152601810cbcb33e1019844967ea6a963d8002b
MarkNuar <marco.caruso98@gmail.com>
2020-10-24 12:46:03 +0200

    Target optimization

--- Assets/HellsChicken/Scripts/Game/Player/Egg/Target.cs
+++ Assets/HellsChicken/Scripts/Game/Player/Egg/Target.cs
@@ -8,16 +8,19 @@ namespace HellsChicken.Scripts.Game.Player.Egg
         [SerializeField] GameObject crosshair;
     
         private static Vector3 _target;
-    
+
+        private Camera _camera;
+        
         // Use this for initialization
         void Start () {
             Cursor.visible = false;
+            _camera = GetComponent<Camera>();
         }
 
         // Update is called once per frame
         void Update()
         {
-            _target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -transform.position.z));
+            _target = _camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -transform.position.z));
 
             crosshair.transform.position = new Vector2(_target.x, _target.y);
         }
