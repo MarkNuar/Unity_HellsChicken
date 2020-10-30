ae06fe43d1e2d43d4b8e718bea980dd01ead9e10
ToMmAzO <tommaso4.pozzi@mail.polimi.it>
2020-10-30 12:17:10 +0100

    Spear and Rock

---
+++ Assets/HellsChicken/Scripts/Game/EnemyObstacles/SpearMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace HellsChicken.Scripts.Game.EnemyObstacles
+{
+    public class SpearMovement : MonoBehaviour
+    {
+
+        private Vector3 _startPosition;
+
+        private float frequency = 5.0f;
+        private float magnitude = 5.0f;
+        private float offset = 0.0f;
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+            _startPosition = transform.position;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.position = _startPosition + transform.up * Mathf.Sin(Time.time * frequency + offset) * magnitude;
+        }
+    }
+}
