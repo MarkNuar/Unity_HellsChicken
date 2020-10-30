3b676b9ef94ab6d09c2cef79375d57c148c7e177
Doppuz <diego.vallauri@mail.com>
2020-10-30 22:42:42 +0100

    The trajectory is ok now but not perfect

--- Assets/HellsChicken/Scripts/Game/AI/Centaur/Bow/CentaurFire.cs
+++ Assets/HellsChicken/Scripts/Game/AI/Centaur/Bow/CentaurFire.cs
@@ -9,17 +9,18 @@ public class CentaurFire : MonoBehaviour {
     
     [SerializeField] private GameObject startExplosion;
     [SerializeField] private GameObject contactExplosion;
-
+    [SerializeField] private float initialVelocity = 20f;
+    
     private Rigidbody _rigidbody;
     private Vector3 target;
-    private float throwForce = 20f;
-    private Vector3 startpos;
-    private float speed = 20f;
-    private float arcHeight = 0.7f;
-    private Vector3 startPos;
-
+    private Vector3 centaurPos;
+    
     public Vector3 Target {
         set => target = value;
+    }
+    
+    public Vector3 CentaurPos {
+        set => centaurPos = value;
     }
     
     private void Awake() {
@@ -27,50 +28,48 @@ public class CentaurFire : MonoBehaviour {
     }
 
     private void Start() {
-        startPos = transform.position;
         Instantiate(startExplosion, transform.position, Quaternion.identity);
         EventManager.TriggerEvent("centaurShot");
-        startpos = _rigidbody.position;
-    }
-
-    //when the bomb goes too far, destroy it.
-    private void Update() {
-        if ((startPos - transform.position).magnitude > 60) {
-            Destroy(gameObject);
-        }
     }
     
-    //Find it on internet
-    private void FixedUpdate() {
-        float x0 = startpos.x;
-        float x1 = target.x;
-        float dist = x1 - x0;
-        float nextX = Mathf.MoveTowards(transform.position.x, x1, speed * Time.deltaTime);
-        float baseY = Mathf.Lerp(startpos.y,  target.y, (nextX - x0) / dist);
-        float arc = arcHeight * (nextX - x0) * (nextX - x1) / (-0.25f * dist * dist);
-        Vector3 nextPos = new Vector3(nextX, baseY + arc, transform.position.z);
-        transform.position = nextPos;
-		
-        if (nextPos == target) {
-            Instantiate(contactExplosion,nextPos, Quaternion.identity);
-            Destroy(gameObject);
-        }
-    }
-
-    /*Second method, Linear.
-     Vector3 calculateDirection() {
-        Vector3 _lookDirection = target - _rigidbody.position;
-        //float angle = Mathf.Atan2(_lookDirection.y, _lookDirection.x) * Mathf.Rad2Deg;
-        Vector2 direction = _lookDirection.normalized;
+    //Search for the angle for the trajectory.
+    public void findAngle(bool right) {
+         float angleCentaurTarget;
+         Vector3 rotatedVector;
+         
+         float g = -Physics.gravity.y;
+         float v2 = initialVelocity * initialVelocity;
+         float v4 = v2 * v2;
+         float x = Mathf.Abs((target.x - centaurPos.x));
+         float y = Mathf.Abs((target.y - centaurPos.y));
+         float x2 = x * x;
+         float squareRoot = (float)Mathf.Sqrt(v4 - g * (g * x2 + 2 * y * v2));
         
-        Debug.DrawLine(gameObject.transform.position, direction, Color.blue, 5f);
-        return direction.normalized;
-    }
+         //This is the angle to be applied from the horizontal vector (but i couldn't get it).
+         float angle = Mathf.Atan((v2 - squareRoot) / (g * x))*Mathf.Rad2Deg;
 
-    public void launch() {
-        Vector3 direction = calculateDirection();
-        _rigidbody.velocity = calculateDirection() * throwForce;
-    }*/
+         //Different angle if i am going right or left.
+         if (!right) { 
+             //Calculate the angle beetween the horizontal vector (here works) and the vector from the centaur to the target.
+            angleCentaurTarget = Mathf.Acos(Vector3.Dot(Vector3.left, (target - centaurPos).normalized)) *
+                      Mathf.Rad2Deg;
+            //Substracted the 2 angle and apply the rotation from the target (not from horizontal).
+            rotatedVector = Quaternion.AngleAxis(angle - angleCentaurTarget, Vector3.back) *
+                            (target - centaurPos); 
+         }else {
+             angleCentaurTarget = Mathf.Acos(Vector3.Dot(Vector3.left, (target - centaurPos).normalized)) *
+                            Mathf.Rad2Deg;
+             angleCentaurTarget = 180 - angleCentaurTarget;
+             rotatedVector = Quaternion.AngleAxis(angle - angleCentaurTarget, Vector3.forward) *
+                                  (target - centaurPos); 
+         }
+
+         rotatedVector += centaurPos;
+        
+         //Apply new velocity
+         _rigidbody.velocity = (rotatedVector - centaurPos).normalized * initialVelocity;
+        
+    }
 
     public void OnCollisionEnter(Collision other) {
         if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Wall")
