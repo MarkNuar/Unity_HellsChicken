e1359c4ead3ef9fc5d57056b56c340c90fe6dd9b
ToMmAzO <tommaso4.pozzi@mail.polimi.it>
2020-10-31 11:27:24 +0100

    Pendulum obstacle

---
+++ Assets/HellsChicken/Scripts/Game/EnemyObstacles/PendulumEngine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+namespace HellsChicken.Scripts.Game.EnemyObstacles
+{
+    public class PendulumEngine : MonoBehaviour
+    {
+
+        private Quaternion _start;
+        private Quaternion _end;
+
+        private float angle = 90.0f;
+        private float speed = 2.0f;
+        
+        [SerializeField] float startTime;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            _start = PendulumRotation(angle);
+            _end = PendulumRotation(-angle);
+        }
+        
+        void FixedUpdate()
+        {
+            startTime += Time.deltaTime;
+            transform.rotation = Quaternion.Lerp(_start, _end, (Mathf.Sin(startTime * speed + Mathf.PI / 2) + 1.0f) / 2.0f);
+        }
+        
+        Quaternion PendulumRotation(float angle)
+        {
+            Quaternion pendulumRotation = transform.rotation;
+            float rotation = pendulumRotation.eulerAngles.z + angle;
+
+            if (rotation > 180)
+            {
+                rotation -= 360;
+            } 
+            if (rotation < -180)
+            {
+                rotation += 360;
+            }
+            
+            pendulumRotation.eulerAngles = new Vector3(pendulumRotation.eulerAngles.x, pendulumRotation.eulerAngles.y, rotation);
+
+            return pendulumRotation;
+        }
+    }
+}
