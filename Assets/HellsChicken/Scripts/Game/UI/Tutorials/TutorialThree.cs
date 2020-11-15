197ba7275201388e018d6417e232af5110fabc2a
Tallino <gdanese@hotmail.it>
2020-11-15 13:27:29 +0100

    tutorial fix

---
+++ Assets/HellsChicken/Scripts/Game/UI/Tutorials/TutorialThree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialThree : MonoBehaviour
+{
+    public GameObject textTutorialTwo;
+    public GameObject textTutorialThree;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("dio cane!");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("dio cane dio!");
+            textTutorialTwo.gameObject.SetActive(false);
+            textTutorialThree.gameObject.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/HellsChicken/Scripts/Game/UI/Tutorials/TutorialTwo.cs
+++ Assets/HellsChicken/Scripts/Game/UI/Tutorials/TutorialTwo.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,11 +9,16 @@ public class TutorialTwo : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.gameObject.tag);
         if (other.gameObject.CompareTag("Player"))
         {   
             textTutorialOne.gameObject.SetActive(false);
             textTutorialTwo.gameObject.SetActive(true);
         }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
