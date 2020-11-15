6344e0be06b73547160ad69ac48761ea5eb12a80
Tallino <gdanese@hotmail.it>
2020-11-15 12:34:11 +0100

    added tutorial triggers and fixed tags of goombas

---
+++ Assets/TutorialTwo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTwo : MonoBehaviour
+{
+    public GameObject textTutorialOne;
+    public GameObject textTutorialTwo;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.tag);
+        if (other.gameObject.CompareTag("Player"))
+        {   
+            textTutorialOne.gameObject.SetActive(false);
+            textTutorialTwo.gameObject.SetActive(true);
+        }
+    }
+}
