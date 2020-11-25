772275fbace28d70732566e34cf8c4c88d19f8ec
Doppuz <diego.vallauri@gmail.com>
2020-11-25 23:52:17 +0100

    Added sky rotation.

---
+++ Assets/HellsChicken/Scripts/Game/SkyBoxRotation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBoxRotation : MonoBehaviour {
+
+    private Material skyBoxMaterial;
+    private float i = 0;
+    
+    private void Awake() {
+        skyBoxMaterial = RenderSettings.skybox;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        i = (i + 0.03f) % 360;
+        skyBoxMaterial.SetFloat("_Rotation",i);
+    }
+}
