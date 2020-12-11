24346466bb03e53ce15f1d3ccca9c1c8803d0896
Tallino <gdanese@hotmail.it>
2020-12-11 18:29:11 +0100

    general fixes

---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class SettingsMenu : MonoBehaviour
+{
+  public AudioMixer audioMixer;
+  
+  public void SetVolume(float volume)
+  { 
+    
+    audioMixer.SetFloat("MusicVolume",volume);
+  }
+
+  public void SetQuality(int qualityIndex)
+  {
+    
+  }
+  
+}
