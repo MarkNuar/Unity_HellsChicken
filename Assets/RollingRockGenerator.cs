dea63181eb3887ac6b289427ac18de28ff994b9c
Tallino <gdanese@hotmail.it>
2020-12-12 22:47:38 +0100

    added rock rolling spawner

---
+++ Assets/RollingRockGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingRockGenerator : MonoBehaviour
+{
+
+    
+    public GameObject rollingRockPrefab;
+    public int rockSpawnInterval = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRocks(rockSpawnInterval));
+    }
+
+
+    IEnumerator SpawnRocks(int timer)
+    {
+        while (true)
+        {
+            Instantiate(rollingRockPrefab, transform.position, transform.rotation);
+            yield return new WaitForSeconds(timer);
+        }
+    }
+}
