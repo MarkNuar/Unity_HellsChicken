878673e0a8d587c8d4ab4b212c7a30ab8496bb1e
Tallino <gdanese@hotmail.it>
2020-12-22 22:45:29 +0100

    initial demon controller

---
+++ Assets/HellsChicken/Scripts/Game/AI/DemonBossController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonBossController : MonoBehaviour
+{
+    [SerializeField] public int _health = 100;
+    public Animator anim;
+
+
+    public void Update()
+    {
+        if (_health == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Attack"))
+        {
+            _health -= 10;
+            Debug.Log(_health);
+        }
+    }
+}
