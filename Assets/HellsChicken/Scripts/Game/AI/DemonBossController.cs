e5e3d13b180b330e45f1f38fb37a622bcbf9d815
Tallino <gdanese@hotmail.it>
2020-12-23 12:14:26 +0100

    demon

--- Assets/HellsChicken/Scripts/Game/AI/DemonBossController.cs
+++ Assets/HellsChicken/Scripts/Game/AI/DemonBossController.cs
@@ -7,10 +7,24 @@ public class DemonBossController : MonoBehaviour
 {
     [SerializeField] public int _health = 100;
     public Animator anim;
+    private CharacterController _characterController;
+    private Vector3 _movement;
 
+
+    public void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    public void Start()
+    {
+        _movement = Vector3.zero;
+        _movement.x = -5f;
+    }
 
     public void Update()
     {
+        _characterController.Move(_movement * Time.deltaTime);
         if (_health == 0)
         {
             Destroy(gameObject);
