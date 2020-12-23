b0ed04e2d53d9f7a5fe2eeccc883ed32f5c810f2
Tallino <gdanese@hotmail.it>
2020-12-23 14:50:50 +0100

    demon

--- Assets/HellsChicken/Scripts/Game/AI/DemonBossController.cs
+++ Assets/HellsChicken/Scripts/Game/AI/DemonBossController.cs
@@ -10,6 +10,11 @@ public class DemonBossController : MonoBehaviour
     private CharacterController _characterController;
     private Vector3 _movement;
 
+    private bool _isMoving;
+    private bool _isDead;
+    private bool _isFighting;
+    private bool _isEnraged;
+
 
     public void Awake()
     {
@@ -19,16 +24,24 @@ public class DemonBossController : MonoBehaviour
     public void Start()
     {
         _movement = Vector3.zero;
-        _movement.x = -2f;
     }
 
     public void Update()
     {
-        _characterController.Move(_movement * Time.deltaTime);
+
+        if (_movement.x != 0)
+            _isMoving = true;
+        else
+            _isMoving = false;
+        
         if (_health == 0)
-        {
-            Destroy(gameObject);
-        }
+            _isDead = true;
+        
+        
+        anim.SetBool("isMoving",_isMoving);
+        anim.SetBool("isDead",_isDead);
+        
+        _characterController.Move(_movement * Time.deltaTime);
     }
 
     public void OnCollisionEnter(Collision other)
@@ -36,7 +49,6 @@ public class DemonBossController : MonoBehaviour
         if (other.gameObject.CompareTag("Attack"))
         {
             _health -= 10;
-            Debug.Log(_health);
         }
     }
 }
