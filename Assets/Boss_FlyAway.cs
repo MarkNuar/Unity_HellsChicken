44bb570373d2e09c89d6e67e8033b7406e424037
Tallino <gdanese@hotmail.it>
2020-12-24 20:58:46 +0100

    demon update

---
+++ Assets/Boss_FlyAway.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_FlyAway : StateMachineBehaviour
+{
+    private GameObject player;
+    private GameObject demonBoss;
+    private CharacterController _bossCharacterController;
+    public float landingCoordinateX;
+    public float flyingSpeed;
+
+    private Vector3 flyAwayTarget;
+    private Vector3 flyAwayMoveVector;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.Find("PlayerBody");
+        demonBoss = GameObject.FindGameObjectWithTag("DemonBoss");
+        _bossCharacterController = demonBoss.GetComponent<CharacterController>();
+        flyAwayTarget = new Vector3(landingCoordinateX, demonBoss.transform.position.y, demonBoss.transform.position.z);
+        flyAwayMoveVector = flyAwayTarget - demonBoss.transform.position;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _bossCharacterController.Move(flyAwayMoveVector * flyingSpeed * Time.deltaTime);
+
+        if(Vector3.Distance(player.transform.position, demonBoss.transform.position) >= landingCoordinateX)
+            animator.SetTrigger("StopFlying");
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+    
+}
--- Assets/Boss_Walk.cs
+++ Assets/Boss_Walk.cs
@@ -6,13 +6,18 @@ public class Boss_Walk : StateMachineBehaviour
 {
     public float speed;
     public float attackRange;
+    public float flyAwayRange;
+    public float whipRange;
     
+    private bool hasStoppedFlying;
     private GameObject player;
     private GameObject demonBoss;
     private CharacterController _bossCharacterController;
     private DemonBossController _demonBossController;
     private GameObject demonBossSword;
-    
+    private Vector3 target;
+    private Vector3 moveVector;
+
     
      //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -23,20 +28,33 @@ public class Boss_Walk : StateMachineBehaviour
         _demonBossController = demonBoss.GetComponent<DemonBossController>();
         demonBossSword = GameObject.Find("DEMON_LORD_SWORD");
         demonBossSword.GetComponent<CapsuleCollider>().enabled = false;
+        hasStoppedFlying = true;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         _demonBossController.LookAtPlayer();
-        Vector3 target = new Vector3(player.transform.position.x, demonBoss.transform.position.y,demonBoss.transform.position.z);
-        Vector3 moveVector = target - demonBoss.transform.position;
+        target = new Vector3(player.transform.position.x, demonBoss.transform.position.y,demonBoss.transform.position.z);
+        moveVector = target - demonBoss.transform.position;
         _bossCharacterController.Move(moveVector * speed * Time.deltaTime);
 
-        if (Vector3.Distance(player.transform.position, demonBoss.transform.position) <= attackRange)
+        if (Vector3.Distance(player.transform.position, demonBoss.transform.position) <= attackRange && Vector3.Distance(player.transform.position, demonBoss.transform.position) >= flyAwayRange)
         {
             demonBossSword.GetComponent<CapsuleCollider>().enabled = true;
             animator.SetTrigger("SwordAttack2");
+        }
+
+        if (Vector3.Distance(player.transform.position, demonBoss.transform.position) <= flyAwayRange && hasStoppedFlying)
+        {
+            animator.SetTrigger("FlyBackwards");
+            hasStoppedFlying = false;
+            Debug.Log("ammerdaaa");
+        }
+
+        if (Vector3.Distance(player.transform.position, demonBoss.transform.position) >= whipRange)
+        {
+            animator.SetTrigger("WhipAttack");
         }
     }
 
@@ -44,6 +62,7 @@ public class Boss_Walk : StateMachineBehaviour
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.ResetTrigger("SwordAttack2");
+        animator.ResetTrigger("FlyBackwards");
     }
     
 }
