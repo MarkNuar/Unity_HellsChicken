7f619fed7ca6c36dc4f92bc773394658e08a4b82
MarkNuar <marco.caruso98@gmail.com>
2020-12-30 15:11:56 +0100

    New Main Menu'

---
+++ Assets/HellsChicken/Scripts/Game/UI/Menu/CameraRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace HellsChicken.Scripts.Game.UI.Menu
+{
+    public class CameraRotation : MonoBehaviour
+    {
+        public Transform lookAtPoint;
+        public Transform rotationCenter;
+        public float cameraRotationSpeed = 2.0f;
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.LookAt(lookAtPoint);
+            transform.RotateAround(rotationCenter.position,Vector3.up, cameraRotationSpeed);
+        }
+    }
+}
